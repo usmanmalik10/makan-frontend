56e1fde0751305f286a35db5c73966c65823b956
usmanmani010 <usmanmani010@gmail.com>
2023-05-27 02:33:53 +0500

    makan makan

--- src/Components/adminpanel/AdminNavbar/Navbar.jsx
+++ src/Components/adminpanel/AdminNavbar/Navbar.jsx
@@ -25,12 +25,124 @@ const Navbar = () => {
       route: "/services",
       submenu: [ // Add submenu for the "Users" menu item
       {
-        text: "Submenu 1",
-        route: "/submenu-1",
+        text: "Mason",
+        route: "/mason",
       },
       {
-        text: "Submenu 2",
-        route: "/submenu-2",
+        text: "Marble/Tile Fixer",
+        route: "/marble/tile-fixer",
+      },
+      {
+        text: "Plumber",
+        route: "/plumber",
+      },
+      {
+        text: "Contractor",
+        route: "/contractor",
+      },
+      {
+        text: "Engineer",
+        route: "/engineer",
+      },
+      {
+        text: "Architect",
+        route: "/architect",
+      },
+      {
+        text: "Carpenter",
+        route: "/carpenter",
+      },
+      {
+        text: "Wood Cutter",
+        route: "/wood-cutter",
+      },
+      {
+        text: "Painter",
+        route: "/painter",
+      },
+      {
+        text: "Polish Man (Furnture)",
+        route: "/polish-man",
+      },
+      {
+        text: "Steel Fixer/Welder",
+        route: "/steel-fixer",
+      },
+      {
+        text: "Steel Polish",
+        route: "/steel-polish",
+      },
+      {
+        text: "Welder",
+        route: "/welder",
+      },
+      {
+        text: "Molding Worker",
+        route: "/molding-worker",
+      },
+      {
+        text: "Leith Machine Operator",
+        route: "/Leith-machine-operator",
+      },
+      {
+        text: "Aluminum Fixer",
+        route: "/aluminum-fixer",
+      },
+      {
+        text: "Glass Fixer",
+        route: "/glass-fixer",
+      },
+      {
+        text: "Glass Cutting & Polish",
+        route: "/glass-cutting-polish",
+      },
+      {
+        text: "Glass Painter",
+        route: "/glass-painter",
+      },
+      {
+        text: "Glass Paper and Designer",
+        route: "/glass-designer",
+      },
+      {
+        text: "Electrician",
+        route: "/electrician",
+      },
+      {
+        text: "AC Technician",
+        route: "/ac-technician",
+      },
+      {
+        text: "Electric Engineer",
+        route: "/electric-engineer",
+      },
+      {
+        text: "Solar Installer",
+        route: "/solar-installer",
+      },
+      {
+        text: "Boring Workers",
+        route: "/boring-workers",
+      },
+      {
+        text: "Interior Designer",
+        route: "/interior-designer",
+      },
+      {
+        text: "Termite Treatment",
+        route: "/termite-treatment",
+      },
+      {
+        text: "Gardener",
+        route: "/gardener",
+      },
+      {
+        text: "Landscape Architect",
+        route: "/landscape-architect",
+      },
+      {
+        text: "House Cleaner",
+        route: "/house-cleaner",
       },
     ],
     },
@@ -40,12 +152,104 @@ const Navbar = () => {
       route: "/stores",
       submenu: [ // Add submenu for the "Users" menu item
       {
-        text: "Submenu 1",
-        route: "/submenu-1",
+        text: "Building Material",
+        route: "/building-material",
       },
       {
-        text: "Submenu 2",
-        route: "/submenu-2",
+        text: "Bricks",
+        route: "/bricks",
+      },
+      {
+        text: "Marble/Tile",
+        route: "/marble-tile",
+      },
+      {
+        text: "Ceramics",
+        route: "/ceramics",
+      },
+      {
+        text: "Sanitary",
+        route: "/sanitary",
+      },
+      {
+        text: "Boring Material",
+        route: "/boring-material",
+      },
+      {
+        text: "Wood",
+        route: "/wood",
+      },
+      {
+        text: "Timber",
+        route: "/timber",
+      },
+      {
+        text: "Paint",
+        route: "/paint",
+      },
+      {
+        text: "Steel",
+        route: "/steel",
+      },
+      {
+        text: "Iron",
+        route: "/iron",
+      },
+      {
+        text: "Aluminum",
+        route: "/aluminum",
+      },
+      {
+        text: "Glass",
+        route: "/glass",
+      },
+      {
+        text: "Hardware",
+        route: "/hardware",
+      },
+      {
+        text: "Electronics",
+        route: "/electronics",
+      },
+      {
+        text: "Electric",
+        route: "/electric",
+      },
+      {
+        text: "Wall Panelling",
+        route: "/wall-panelling",
+      },
+      {
+        text: "Solar System",
+        route: "/solar-system",
+      },
+      {
+        text: "Nursery",
+        route: "/nursery",
+      },
+      {
+        text: "Concrete Plant",
+        route: "/concrete-plant",
+      },
+      {
+        text: "Interior Decoration",
+        route: "/interior-decoration",
+      },
+      {
+        text: "Lights",
+        route: "/lights",
+      },
+      {
+        text: "Furniture",
+        route: "/furniture",
+      },
+      {
+        text: "Security",
+        route: "/security",
+      },
+      {
+        text: "Termite Protection",
+        route: "/termite-protection",
       },
     ],
     },
