6d027b25a648eb9f8e0ebde01db24fbdd90c4dba
usmanmalik10 <usmanmaliksahib2611@gmail.com>
2024-01-10 22:27:41 +0500

    commit

--- src/Components/constants/config/config.dev.js
+++ src/Components/constants/config/config.dev.js
@@ -1,70 +1,65 @@
 // const host = window.location.origin;
 
-  export const USERS_BASE_URL = "https://www.makanmanager.com/api";
+export const USERS_BASE_URL = "https://www.makanmanager.com/api";
 
-//  export const USERS_BASE_URL = "http://localhost:4000";  
-
-
-
-
+// export const USERS_BASE_URL = 'http://localhost:4000';
 
 export const homeforrent = {
-    "category": "house",
-    "subCategory": "rent",
-    "address": "12 D wafaqi colony",
-    "location": "Lahore",
-    "size": "5 kanal",
-    "contectNumber": "03231212321",
-    "bedRooms": 3,
-    "price": "100,000000",
-    "details": {
-        "story": 2,
-        "bathromm": 4
-  }
+  category: 'house',
+  subCategory: 'rent',
+  address: '12 D wafaqi colony',
+  location: 'Lahore',
+  size: '5 kanal',
+  contectNumber: '03231212321',
+  bedRooms: 3,
+  price: '100,000000',
+  details: {
+    story: 2,
+    bathromm: 4,
+  },
 };
 
-  export const homeforsale = {
-    "category": "house",
-    "subCategory": "sale",
-    "address": "12 D wafaqi colony",
-    "location": "Lahore",
-    "size": "5 kanal",
-    "contectNumber": "03231212321",
-    "bedRooms": 3,
-    "price": "100,000000",
-    "details": {
-        "story": 2,
-        "bathromm": 4
-  }
+export const homeforsale = {
+  category: 'house',
+  subCategory: 'sale',
+  address: '12 D wafaqi colony',
+  location: 'Lahore',
+  size: '5 kanal',
+  contectNumber: '03231212321',
+  bedRooms: 3,
+  price: '100,000000',
+  details: {
+    story: 2,
+    bathromm: 4,
+  },
 };
 
 export const plotforrent = {
-    "category": "plot",
-    "subCategory": "rent",
-    "address": "12 D wafaqi colony",
-    "location": "Lahore",
-    "size": "5 kanal",
-    "contectNumber": "03231212321",
-    "bedRooms": 3,
-    "price": "100,000000",
-    "details": {
-        "story": 2,
-        "bathromm": 4
-  }
+  category: 'plot',
+  subCategory: 'rent',
+  address: '12 D wafaqi colony',
+  location: 'Lahore',
+  size: '5 kanal',
+  contectNumber: '03231212321',
+  bedRooms: 3,
+  price: '100,000000',
+  details: {
+    story: 2,
+    bathromm: 4,
+  },
 };
 
-  export const plotforsale = {
-    "category": "plot",
-    "subCategory": "sale",
-    "address": "12 D wafaqi colony",
-    "location": "Lahore",
-    "size": "5 kanal",
-    "contectNumber": "03231212321",
-    "bedRooms": 3,
-    "price": "100,000000",
-    "details": {
-        "story": 2,
-        "bathromm": 4
-  }
+export const plotforsale = {
+  category: 'plot',
+  subCategory: 'sale',
+  address: '12 D wafaqi colony',
+  location: 'Lahore',
+  size: '5 kanal',
+  contectNumber: '03231212321',
+  bedRooms: 3,
+  price: '100,000000',
+  details: {
+    story: 2,
+    bathromm: 4,
+  },
 };
-
